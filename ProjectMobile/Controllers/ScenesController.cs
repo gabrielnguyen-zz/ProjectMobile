d438108b4d03855f8eda04a8e049a6d8602e936f
gabrielnguyen-zz <59083327+gabrielnguyen-zz@users.noreply.github.com>
2020-06-22 13:15:41 +0700

    test

---
+++ ProjectMobile/Controllers/ScenesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectMobile.Models;
+
+namespace ProjectMobile
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScenesController : ControllerBase
+    {
+        private readonly ProjectMobileContext _context;
+
+        public ScenesController(ProjectMobileContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Scenes
+        [HttpGet]
+        public IEnumerable<Scene> GetScene()
+        {
+            return _context.Scene;
+        }
+
+        // GET: api/Scenes/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetScene([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var scene = await _context.Scene.FindAsync(id);
+
+            if (scene == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(scene);
+        }
+
+        // PUT: api/Scenes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutScene([FromRoute] int id, [FromBody] Scene scene)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != scene.SceneId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(scene).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SceneExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Scenes
+        [HttpPost]
+        public async Task<IActionResult> PostScene([FromBody] Scene scene)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Scene.Add(scene);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetScene", new { id = scene.SceneId }, scene);
+        }
+
+        // DELETE: api/Scenes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteScene([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var scene = await _context.Scene.FindAsync(id);
+            if (scene == null)
+            {
+                return NotFound();
+            }
+
+            _context.Scene.Remove(scene);
+            await _context.SaveChangesAsync();
+
+            return Ok(scene);
+        }
+
+        private bool SceneExists(int id)
+        {
+            return _context.Scene.Any(e => e.SceneId == id);
+        }
+    }
+}
---
+++ ProjectMobile/VModel/ActorVModel.cs
@@ -0,0 +1,13 @@
+﻿using ProjectMobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectMobile.VModel
+{
+    public class ActorVModel : Actor
+    {
+        public string password { get; set; }
+    }
+}
